66fda845f9f737774796f8d5c1c0a1dd4776e372
RodrigoNande <rodricampos882@gmail.com>
2025-08-24 12:42:28 -0600

    update

--- sql/V3_datos_categoriasproductos.sql
+++ sql/V3_datos_categoriasproductos.sql
@@ -1,9 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.2
--- https://www.phpmyadmin.net/
---
--- Servidor: mysql
--- Tiempo de generación: 24-08-2025 a las 18:32:21
+- Tiempo de generación: 24-08-2025 a las 18:39:55
 -- Versión del servidor: 8.0.42
 -- Versión de PHP: 8.2.27
 
@@ -92,77 +87,6 @@ INSERT INTO `Categoria` (`Id`, `Nombre_Categoria`, `Descripcion`, `IdCategoriaPa
 ('REGALOS', 'Regalos y Accesorios', 'Artículos de regalo y decorativos', NULL),
 ('TECNOLOGIA', 'Tecnología', 'Artículos tecnológicos básicos', NULL);
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `detalle_venta`
---
-
-CREATE TABLE `detalle_venta` (
-  `IdDetalle` int NOT NULL,
-  `IdVenta` varchar(100) NOT NULL,
-  `IdProducto` varchar(100) NOT NULL,
-  `Cantidad` int NOT NULL,
-  `PrecioUnitario` decimal(10,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `flyway_schema_history`
---
-
-CREATE TABLE `flyway_schema_history` (
-  `installed_rank` int NOT NULL,
-  `version` varchar(50) DEFAULT NULL,
-  `description` varchar(200) NOT NULL,
-  `type` varchar(20) NOT NULL,
-  `script` varchar(1000) NOT NULL,
-  `checksum` int DEFAULT NULL,
-  `installed_by` varchar(100) NOT NULL,
-  `installed_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `execution_time` int NOT NULL,
-  `success` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Volcado de datos para la tabla `flyway_schema_history`
---
-
-INSERT INTO `flyway_schema_history` (`installed_rank`, `version`, `description`, `type`, `script`, `checksum`, `installed_by`, `installed_on`, `execution_time`, `success`) VALUES
-(1, '1', 'crear tablas', 'SQL', 'V1__crear_tablas.sql', -1350077460, 'app_user', '2025-08-19 03:08:08', 515, 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `img`
---
-
-CREATE TABLE `img` (
-  `idProd` varchar(50) NOT NULL,
-  `ruta` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Volcado de datos para la tabla `img`
---
-
-INSERT INTO `img` (`idProd`, `ruta`) VALUES
-('0001', 'img/BoligrafoBic.webp'),
-('0002', 'img/CajadePapelBond.webp'),
-('0003', 'img/cuaderno1.webp');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `producto_categoria`
---
-
-CREATE TABLE `producto_categoria` (
-  `Id_Producto` varchar(100) NOT NULL,
-  `Id_Categoria` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 --
 -- Volcado de datos para la tabla `producto_categoria`
 --
@@ -176,37 +100,6 @@ INSERT INTO `producto_categoria` (`Id_Producto`, `Id_Categoria`) VALUES
 ('ESC003', 'ESCOLAR_GEOMETRIA'),
 ('OFC001', 'OFICINA_ARCHIVO'),
 ('OFC002', 'OFICINA_HERRAMIENTAS');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `IdUsuario` varchar(100) NOT NULL,
-  `Nombre` varchar(100) NOT NULL,
-  `Apellido` varchar(100) NOT NULL,
-  `Correo` varchar(100) NOT NULL,
-  `Telefono` varchar(20) NOT NULL,
-  `Direccion` varchar(100) NOT NULL,
-  `Usuario` varchar(100) NOT NULL,
-  `Contrasena` varchar(255) NOT NULL,
-  `Rol` enum('cliente','admin') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `ventas`
---
-
-CREATE TABLE `ventas` (
-  `IdVenta` varchar(100) NOT NULL,
-  `IdUsuario` varchar(100) NOT NULL,
-  `Fecha` datetime NOT NULL,
-  `Total` decimal(10,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Índices para tablas volcadas
@@ -226,59 +119,6 @@ ALTER TABLE `Categoria`
   ADD KEY `IdCategoriaPadre` (`IdCategoriaPadre`);
 
 --
--- Indices de la tabla `detalle_venta`
---
-ALTER TABLE `detalle_venta`
-  ADD PRIMARY KEY (`IdDetalle`),
-  ADD KEY `IdProducto` (`IdProducto`),
-  ADD KEY `IdVenta` (`IdVenta`);
-
---
--- Indices de la tabla `flyway_schema_history`
---
-ALTER TABLE `flyway_schema_history`
-  ADD PRIMARY KEY (`installed_rank`),
-  ADD KEY `flyway_schema_history_s_idx` (`success`);
-
---
--- Indices de la tabla `img`
---
-ALTER TABLE `img`
-  ADD KEY `idProd` (`idProd`);
-
---
--- Indices de la tabla `producto_categoria`
---
-ALTER TABLE `producto_categoria`
-  ADD KEY `Id_Producto` (`Id_Producto`),
-  ADD KEY `Id_Categoria` (`Id_Categoria`);
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`IdUsuario`),
-  ADD UNIQUE KEY `Correo` (`Correo`),
-  ADD UNIQUE KEY `Usuario` (`Usuario`);
-
---
--- Indices de la tabla `ventas`
---
-ALTER TABLE `ventas`
-  ADD PRIMARY KEY (`IdVenta`),
-  ADD KEY `IdUsuario` (`IdUsuario`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `detalle_venta`
---
-ALTER TABLE `detalle_venta`
-  MODIFY `IdDetalle` int NOT NULL AUTO_INCREMENT;
-
---
 -- Restricciones para tablas volcadas
 --
 
@@ -287,32 +127,6 @@ ALTER TABLE `detalle_venta`
 --
 ALTER TABLE `Categoria`
   ADD CONSTRAINT `Categoria_ibfk_1` FOREIGN KEY (`IdCategoriaPadre`) REFERENCES `Categoria` (`Id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Filtros para la tabla `detalle_venta`
---
-ALTER TABLE `detalle_venta`
-  ADD CONSTRAINT `detalle_venta_ibfk_1` FOREIGN KEY (`IdVenta`) REFERENCES `ventas` (`IdVenta`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `detalle_venta_ibfk_2` FOREIGN KEY (`IdProducto`) REFERENCES `articulo` (`IdProducto`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Filtros para la tabla `img`
---
-ALTER TABLE `img`
-  ADD CONSTRAINT `img_ibfk_1` FOREIGN KEY (`idProd`) REFERENCES `articulo` (`IdProducto`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Filtros para la tabla `producto_categoria`
---
-ALTER TABLE `producto_categoria`
-  ADD CONSTRAINT `producto_categoria_ibfk_1` FOREIGN KEY (`Id_Categoria`) REFERENCES `Categoria` (`Id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `producto_categoria_ibfk_2` FOREIGN KEY (`Id_Producto`) REFERENCES `articulo` (`IdProducto`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Filtros para la tabla `ventas`
---
-ALTER TABLE `ventas`
-  ADD CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`IdUsuario`) REFERENCES `usuarios` (`IdUsuario`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
